0ea25f4b630c918fc8d0aead9bb75b58f5af5673
Janek <zlaasu@gmail.com>
2019-10-10 16:55:52 +0200

    Add fonts and docker

--- html/js/ownFunctions.js
+++ html/js/ownFunctions.js
@@ -1,19 +1,33 @@
+var API_URL = "https://workshopsimplecrud-uat.zlasu.org/backend/";
+//var API_URL = "http://localhost:8080/backend/";
 
-
-$(document).ready(function(){
-    $.get("sections/header.html", function(data) {
-        $("#header").html(data);
+$(function(){
+    var includes = $('[data-include]');
+    jQuery.each(includes, function(){
+        var file = 'views/' + $(this).data('include') + '.html';
+        $(this).load(file);
     });
 });
 
-$(document).ready(function(){
-    $.get("sections/footer.html", function(data) {
-        $("#header").html(data);
-    });
-});
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
-$(document).ready(function () {
-    $("div[data-includeHTML]").each(function () {
-        $(this).load($(this).attr("data-includeHTML"));
-    });
-});
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+        end = new Date().getTime();
+    }
+}
