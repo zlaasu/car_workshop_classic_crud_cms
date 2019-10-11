a2edb44635a7a66c2d950e353d7535b5d406286e
Janek <zlaasu@gmail.com>
2019-10-11 10:37:45 +0200

    Add vehicle Add and Edit

--- html/js/ownFunctions.js
+++ html/js/ownFunctions.js
@@ -1,6 +1,7 @@
 var API_URL = "https://workshopsimplecrud-uat.zlasu.org/backend/";
 //var API_URL = "http://localhost:8080/backend/";
 
+
 $(function(){
     var includes = $('[data-include]');
     jQuery.each(includes, function(){
